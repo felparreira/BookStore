0fbb333fd215964c2d48ef8b75e0d6151f88f653
Felipe Parreira <felipepbzr@gmail.com>
2023-06-16 09:50:18 -0300

    first commit

---
+++ aspnet-core/src/Acme.BookStore.MongoDB/MongoDb/BookStoreMongoDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+using Acme.BookStore.Books;
+using MongoDB.Driver;
+using Acme.BookStore.Authors;
+
+namespace Acme.BookStore.MongoDB;
+
+[ConnectionStringName("Default")]
+public class BookStoreMongoDbContext : AbpMongoDbContext
+{
+    /* Add mongo collections here. Example:
+     * public IMongoCollection<Question> Questions => Collection<Question>();
+     */
+        public IMongoCollection<Book> Books => Collection<Book>();
+        //...
+        public IMongoCollection<Author> Authors => Collection<Author>();
+
+    protected override void CreateModel(IMongoModelBuilder modelBuilder)
+    {
+        base.CreateModel(modelBuilder);
+
+        //modelBuilder.Entity<YourEntity>(b =>
+        //{
+        //    //...
+        //});
+    }
+}
